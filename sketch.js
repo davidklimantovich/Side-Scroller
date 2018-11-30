1949e168174921837bc4528a3d7897e4dc30a0a1
davidklimantovich <42980073+davidklimantovich@users.noreply.github.com>
2018-11-30 13:14:50 -0500

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,20 @@
+// Daniel Shiffman
+// https://www.kadenze.com/courses/the-nature-of-code
+// http://natureofcode.com/
+// Session 2: Array of Particles, multiple forces
+var person;
+function setup() {
+  createCanvas(640, 640);
+  person = new Person();
+  
+}
+
+function draw() {
+  background(51);
+ person.update();
+ person.display();
+  
+  
+  fill(124,124,124,124);
+  rect(400,height-50,50,50);
+  }
